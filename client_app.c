0df127fe4d571644ce9b86a47653e0ac12ec53e9
Lee <andy2@sp18-cs241-023.cs.illinois.edu>
2018-04-13 19:17:36 -0500

    Client app changes

--- client_app.c
+++ client_app.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define WRITE_BUFF_SIZE 4096
+#define SERVER_PORT "8000"
+#define SERVER_NAME "localhost"
+
 void print_usage() {
 	fprintf(stdout, "USAGE: ./dist_file <COMMAND> <FILENAME>\nValid <COMMAND>s: push, pull, remove\n");
 }
@@ -20,6 +24,18 @@ int main(int argc, char** argv) {
 
 	if (!strcmp(command, "push")) {
 		fprintf(stdout, "Pushing file: %s\n", filename);
+		FILE* push_file = fopen(filename, "r");
+		fseek(push_file, 0, SEEK_END);
+		size_t total_bytes = ftell(push_file);
+		rewind(push_file);
+		size_t bytes_sent = 0;
+		while (bytes_sent < push_file) {
+			char write_buff[WRITE_BUFF_SIZE];
+			size_t read_size = fread(write_buff, 1, WRITE_BUFF_SIZE, filename);
+			int sent_batch_size = network_send(write_buff, read_size, SERVER_PORT, SERVER_NAME, read_size);
+			bytes_sent = bytes_sent + sent_batch_size;
+			fseek(local, sent_batch_size, SEEK_SET);
+		}
 	}
 	else if (!strcmp(command, "pull")) {
 		fprintf(stdout, "Pulling file: %s\n", filename);
