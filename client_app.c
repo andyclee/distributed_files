f6b7e14c0cb83113650847461abec9c3b270c92b
Li <yankail2@sp18-cs241-307.cs.illinois.edu>
2018-04-27 18:03:57 -0500

    client app changed

--- client_app.c
+++ client_app.c
@@ -12,38 +12,74 @@
 #define SERVER_NAME "localhost"
 
 void print_usage() {
-	fprintf(stdout, "USAGE: ./dist_file <COMMAND> <FILENAME>\nValid <COMMAND>s: push, pull, remove\n");
+	fprintf(stdout, "USAGE: ./dist_file <COMMAND> <FILENAME>\nValid <COMMAND>s: upload, download, list\n");
 }
 
 int main(int argc, char** argv) {
-	if (argc < 3) {
+	if (argc < 3 || argc > 3) {
 		print_usage();
 		return 1;
 	}
 	char* command = argv[1];
 	char* filename = argv[2];
-
-	if (!strcmp(command, "push")) {
-		fprintf(stdout, "Pushing file: %s\n", filename);
+	
+	if (!strcmp(command, "upload")) {
+		fprintf(stdout, "Uploading file %s...\n", filename);
 		FILE* push_file = fopen(filename, "r");
+		if(push_file == NULL) {
+			fprintf(stdout, "Failed to find file %sn", filename);
+			print_usage();
+			return 1;
+		}
+
 		fseek(push_file, 0, SEEK_END);
 		size_t total_bytes = ftell(push_file);
-		rewind(push_file);
-		size_t bytes_sent = 0;
-		while (bytes_sent < total_bytes) {
-			char write_buff[WRITE_BUFF_SIZE];
-			size_t read_size = fread(write_buff, 1, WRITE_BUFF_SIZE, push_file);
-			int sent_batch_size = 
-				network_send(write_buff, filename, SERVER_PORT, SERVER_NAME, read_size);
-			bytes_sent = bytes_sent + sent_batch_size;
-			fseek(push_file, sent_batch_size, SEEK_SET);
+		char* buffer = malloc(total_bytes);
+		
+		fseek(push_file, 0, SEEK_SET);
+		fread(buffer, 1, total_bytes, push_file);
+
+		int result = network_send(buffer, filename, SERVER_PORT, SERVER_NAME, total_bytes);
+		free(buffer);
+		if(result < 0) {
+		    	fprintf(stdout, "Failed to upload file %s, please try again.\n", filename);
+			return 1;
+		} else {
+		    	fprintf(stdout, "Successfully upload file %s\n", filename);
+		}	
+		fclose(push_file);	
+	}
+	else if (!strcmp(command, "download")) {
+		fprintf(stdout, "Downloading file %s...\n", filename);
+		size_t file_size = 0;
+		char* buffer = network_receive(filename, SERVER_PORT, SERVER_NAME, &file_size);
+		if(buffer == NULL) {
+			fprintf(stdout, "Failed to download file %s, please try again.\n", filename);
+			return 1;
+		} 
+		FILE* pull_file = fopen(filename, "w");
+		if(pull_file == NULL) {
+			fprintf(stdout, "Succeed in downloading, failed to write to file %s\n", filename);
+			free(buffer);
+			return 1;
+		} else {
+			fputs(buffer, pull_file);
+			fclose(pull_file);
+			free(buffer);
+			fprintf(stdout, "Successfully download file %s\n", filename);
 		}
 	}
-	else if (!strcmp(command, "pull")) {
-		fprintf(stdout, "Pulling file: %s\n", filename);
-	}
-	else if (!strcmp(command, "remove")) {
-		fprintf(stdout, "Removing file: %s\n", filename);
+	else if (!strcmp(command, "list")) {
+		network_send_list(SERVER_PORT, SERVER_NAME);
+		size_t list_size = 0;
+		char* list = network_receive_list(SERVER_PORT, SERVER_NAME, &list_size);
+		if(list == NULL) {
+			fprintf(stdout, "Failed to get list, please try again.\n");
+			return 1;
+		} 
+		fprintf(stdout, "List of file:\n");
+		fprintf(stdout, "%s", list);
+		free(list);
 	}
 	else {
 		fprintf(stdout, "Invalid command!\n");
